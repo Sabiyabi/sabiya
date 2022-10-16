e307ad556d446b19edd34143885a789ac60023d7
patel <patelsabiya111@gmail.com>
2022-10-16 15:02:01 +0530

    prgm added

---
+++ basic.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+	int x;
+	x=90;
+	main()
+{
+//	int x=50;
+       {
+	x=40;
+	int x=23;
+	printf("%d\n",x);
+	x=70;
+       }    
+	
+      printf("%d\n",x);
+      {
+	      printf("%5d\n %-5d\n",123, 123);//field width
+	      printf("\"patel\"");
+	      printf("%f\n",12.3);
+	      printf("%d\n",12.3);
+	      printf("%10.3f",12.3);
+	      printf("%d\n","output is", printf("%d\n","best"));
+	   /*   int x;
+	      printf("enter value\n");
+	      scanf("%d",&x);
+	    
+	      printf("%d %x %o\n",x, x,x);
+	      char str;
+	      printf("enter the string\n");
+	      gets(str);
+	      printf("%s",str);*/
+
+    }
+      unsigned int x=-1;
+      printf("%d\n",x<1);
+      printf("%d\n",(int)x<1);
+}
+
+
+
